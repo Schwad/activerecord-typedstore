55db971e7522d9a807d3e6216dd67c29f9daf6c6
Nick Schwaderer <nick.schwaderer@shopify.com>
2024-09-10 15:54:13 +0100

    Another approach

--- lib/active_record/typed_store/attribute_assignment_extension.rb
+++ lib/active_record/typed_store/attribute_assignment_extension.rb
@@ -4,12 +4,17 @@ module ActiveRecord
   module TypedStore
     module AttributeAssignmentExtension
       def _assign_attributes(attributes)
+        # In the event that the attributes hash contains keys that are also keys in the typed_stores hash,
+        # we should prioritize the attributes hash.
         attributes.each do |k, v|
           key = k.to_s
-
           if v.is_a?(Hash) && typed_store?(key)
-            _assign_attribute(key, v)
-            attributes.delete(k)
+            v_keys_symbols = v.keys.map(&:to_sym)
+            matching_keys = attributes.keys.select { |attr_key| v_keys_symbols.include?(attr_key.to_sym) }
+
+            matching_keys.each do |matching_key|
+              v[matching_key.to_s] = attributes[matching_key]
+            end
           end
         end
 
